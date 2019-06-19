f81bedabb210f07f17887d5cad8b18d91a7a834e
NicoNico <niconicomellis@gmail.com>
2019-06-18 20:42:20 -0400

    tarea2 prccproch

--- proc.h
+++ proc.h
@@ -36,7 +36,7 @@ enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
 struct proc {
-  int tickets=4.5;                 // numero de tickets del proceso(tarea2)
+  int tickets=4;                 // numero de tickets del proceso(tarea2)
   uint sz;                     // Size of process memory (bytes)
   pde_t* pgdir;                // Page table
   char *kstack;                // Bottom of kernel stack for this process
